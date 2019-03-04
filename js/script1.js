d7a2161dad9f21bea94cd6e0e0d56dc50a53276b
satyam tomar <tomarsatyam49@gmail.com>
2019-03-04 18:20:20 +0530

    blog

---
+++ js/script1.js
@@ -0,0 +1,35 @@
+function headingRepeat(){
+$(document).ready(function(){
+  $(".heading-linux").addClass("bounceInLeft");
+  $("#two-buttons-first").addClass("bounceInRight");
+  $("#two-buttons-second").addClass("bounceInLeft");
+});
+}
+var mobmenu=1;
+function mobilemenu(){
+  if (mobmenu===1)
+  {
+      $(".menu-mobile-content").css("visibility","visible");
+      mobmenu=2;
+   }
+else if (mobmenu===2)
+{
+        $(".menu-mobile-content").css("visibility","hidden");
+        mobmenu=1;
+
+}
+}
+if(screen.width>=800){
+  $(document).ready(function(){
+    headingRepeat();
+  });
+}
+else if(screen.width<=500){
+  $(document).ready(function(){
+    headingRepeat();
+    $(".menu-mobile-btn").click(function(){
+      mobilemenu();
+      });
+    
+    });
+  }
