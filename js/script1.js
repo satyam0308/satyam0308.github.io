f5d19895a3816b14b73679bc415a1d33b07cd8d7
satyam tomar <tomarsatyam49@gmail.com>
2019-03-05 15:32:24 +0530

    making final frontend

--- js/script1.js
+++ js/script1.js
@@ -7,29 +7,36 @@ $(document).ready(function(){
 }
 var mobmenu=1;
 function mobilemenu(){
-  if (mobmenu===1)
-  {
-      $(".menu-mobile-content").css("visibility","visible");
-      mobmenu=2;
-   }
-else if (mobmenu===2)
-{
-        $(".menu-mobile-content").css("visibility","hidden");
-        mobmenu=1;
+        if (mobmenu===1)
+        {
+            $(".menu-mobile-content").css("visibility","visible");
+            mobmenu=2;
+         }
+      else if (mobmenu===2)
+      {
+              $(".menu-mobile-content").css("visibility","hidden");
+              mobmenu=1;
 
-}
-}
+      }
+      }
+
+
+
 if(screen.width>=800){
+
   $(document).ready(function(){
     headingRepeat();
   });
+
 }
-else if(screen.width<=500){
+else if(screen.width<=800){
+
   $(document).ready(function(){
     headingRepeat();
     $(".menu-mobile-btn").click(function(){
       mobilemenu();
       });
-    
+
     });
-  }
+
+}
